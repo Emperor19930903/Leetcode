ead03caa258b0689fd716cfff47e9c824349af9c
Emperor19930903 <46704582+Emperor19930903@users.noreply.github.com>
2022-10-20 16:00:24 +0800

    Create 142-环形链表II.py

---
+++ Linked List/Double Pointer/142-环形链表II.py
@@ -0,0 +1,24 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head or not head.next:
+            return None
+        slow=head
+        fast=head.next
+        while slow!=fast:
+            if not fast or not fast.next:
+                return None
+            slow=slow.next
+            fast=fast.next.next
+        slow=head
+        fast=fast.next    # 补足少走的那一步
+        while slow!=fast:
+            slow=slow.next
+            fast=fast.next
+       
+        return slow
