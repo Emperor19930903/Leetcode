9a0b2859e0a18a1560fe6d2da4ad4692a8e3be92
Emperor19930903 <46704582+Emperor19930903@users.noreply.github.com>
2023-02-21 11:10:01 +0800

    Update 83_删除排序链表中的重复元素.py

--- Linked List/Double Pointer/83_删除排序链表中的重复元素.py
+++ Linked List/Double Pointer/83_删除排序链表中的重复元素.py
@@ -8,5 +8,6 @@ class Solution:
                 
             else:
                 fast = fast.next
-                slow.next=None     # 取消后面重复的链表
+                slow.next=None     # 断开与后面重复元素的连接
         return node    
+# 像 Java/Python 这类带有垃圾回收的语言，可以帮我们自动找到并回收这些「悬空」的链表节点的内存，而像 C++ 这类语言没有自动垃圾回收的机制，确实需要我们编写代码时手动释放掉这些节点的内存。
