509041ff066a5c091b01f0f7c05b75d38a6c4ea4
Emperor <46704582+Emperor19930903@users.noreply.github.com>
2023-03-17 17:09:07 +0800

    Create 49-字母异位词组.c

---
+++ hashTable/49-字母异位词组.c
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        unordered_set<char> set;
+        int left=0,right=0,res=1;
+        int n=s.size();
+        if (n==0)
+            return 0;
+        while(right<s.size()-1)
+        {
+            set.insert(s[right++]);
+            while(set.count(s[right]))
+                set.erase(s[left++]);
+            res=max(res,right-left+1);
+        }
+        return res;
+
+    }
+};
