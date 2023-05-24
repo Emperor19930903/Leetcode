67e790c9fe416376e81ec2c7660878776d030276
Emperor <46704582+Emperor19930903@users.noreply.github.com>
2023-05-24 17:02:36 +0800

    Update 322-零钱兑换.cpp

--- DP/322-零钱兑换.cpp
+++ DP/322-零钱兑换.cpp
@@ -1,3 +1,15 @@
+//给你一个整数数组 coins ，表示不同面额的硬币；以及一个整数 amount ，表示总金额。
+
+//计算并返回可以凑成总金额所需的 最少的硬币个数 。如果没有任何一种硬币组合能组成总金额，返回 -1 。
+
+//你可以认为每种硬币的数量是无限的。
+
+// 示例 1：
+
+// 输入：coins = [1, 2, 5], amount = 11
+// 输出：3 
+// 解释：11 = 5 + 5 + 1
+
 class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
@@ -17,3 +29,5 @@ public:
         return dp[amount]>amount?-1:dp[amount];
     }
 };
+// 时间复杂度：O(Sn)O(Sn)O(Sn)，其中 SSS 是金额，nnn 是面额数。我们一共需要计算 O(S)O(S)O(S) 个状态，SSS 为题目所给的总金额。对于每个状态，每次需要枚举 nnn 个面额来转移状态，所以一共需要 O(Sn)O(Sn)O(Sn) 的时间复杂度。
+// 空间复杂度：O(S)O(S)O(S)。数组 dp\textit{dp}dp 需要开长度为总金额 SSS 的空间。
